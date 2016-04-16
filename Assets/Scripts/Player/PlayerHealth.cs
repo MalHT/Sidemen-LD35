865f5e0ece24337da77be19a00ddf748753cf58a
Xp7100 <george.xp7100@gmail.com>
2016-04-16 14:45:00 +0100

    Update

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,11 +5,13 @@ public class PlayerHealth : MonoBehaviour {
     public int health;
 	// Use this for initialization
 	void Start () {
-	
+        health = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (health <= 0) {
+        }
 	
 	}
 }
