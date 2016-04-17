e4b1981183682af556c5969d98c8434bf0e2bbe2
Mal Hall-Thorpe <me@mal.im>
2016-04-17 13:56:58 +0100

    Level management working!

---
+++ Assets/ExitTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitTrigger : MonoBehaviour {
+
+    public GameManager gameManager;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        // if collides with player (todo)
+
+        gameManager.nextLevel();
+
+    }
+
+    void Start()
+    {
+
+        GameObject gameManagerObject = GameObject.Find("GameManager");
+        gameManager = gameManagerObject.GetComponent<GameManager>();
+
+    }
+
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,9 +5,23 @@ public class GameManager : MonoBehaviour {
 
     public LevelManager levelManager;
 
-    private int level;
+    private int level = 1;
+
+    public void nextLevel()
+    {
+        level = level + 1;
+
+        Debug.Log("Next level is " + level);
+        Debug.Log(levelManager);
+
+        levelManager.makeLevelNumber(level);
+
+        
+
+    }
 
 	// Use this for initialization
+
 	void Awake () {
 
         levelManager = GetComponent<LevelManager>();
